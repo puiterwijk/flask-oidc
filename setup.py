5d9506a802e0793568d9f305880e8a2379255077
Jeremy Ehrhardt <jeremy@bat-country.us>
2014-07-31 16:46:15 -0700

    Packaged 0.1.0

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from pip.req import parse_requirements
 setup(
     name='flask-oidc',
     description='OpenID Connect extension for Flask',
+    url='https://github.com/SteelPangolin/flask-oidc',
+    author='Jeremy Ehrhardt',
+    author_email='jeremy@bat-country.us',
     version='0.1.0',
     packages=[
         'flask_oidc',
