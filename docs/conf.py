5cdd2abdb0452b34d14158b388225f4955a85fd8
Patrick Uiterwijk <patrick@puiterwijk.org>
2016-05-23 23:58:39 +0000

    Use the Flask template
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,8 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.append(os.path.abspath('_themes'))
+sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ------------------------------------------------
 
@@ -112,15 +113,18 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'flask_small'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'index_logo':       'flask-openid.png',
+    'github_fork':      'puiterwijk/flask-oidc'
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
--- setup.py
+++ setup.py
@@ -3,6 +3,18 @@ import io
 
 from setuptools import setup
 
+# This check is to make sure we checkout docs/_themes before running sdist
+if not os.path.exists("./docs/_themes/README"):
+    print('Please make sure you have docs/_themes checked out while running setup.py!')
+    if os.path.exists('.git'):
+        print('You seem to be using a git checkout, please execute the following commands to get the docs/_themes directory:')
+        print(' - git submodule init')
+        print(' - git submodule update')
+    else:
+        print('You seem to be using a release. Please use the release tarball from PyPI instead of the archive from GitHub')
+    sys.exit(1)
+
+
 here = os.path.abspath(os.path.dirname(__file__))
 with io.open(os.path.join(here, 'README.rst')) as f:
     readme = f.read()
