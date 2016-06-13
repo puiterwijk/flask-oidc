2c66a0dcccd8a587c7c4d25979107deb99d71c02
Patrick Uiterwijk <patrick@puiterwijk.org>
2016-06-13 08:30:25 +0000

    Remove vendor-specific url components in discovery url
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- flask_oidc/discovery.py
+++ flask_oidc/discovery.py
@@ -39,5 +39,5 @@ def discover_OP_information(OP_uri):
     .. versionadded:: 1.0
     """
     _, content = httplib2.Http().request(
-        '%s/openidc/.well-known/openid-configuration' % OP_uri)
+        '%s/.well-known/openid-configuration' % OP_uri)
     return json.loads(content)
