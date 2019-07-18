1109003a820999ebb5db39a9f809ba80bd9b9fe5
3Lu <markus.scholl@prodyna.com>
2019-07-18 14:44:20 +0200

    - add 2 new helper functions to extract realm and client roles from token

--- flask_oidc/__init__.py
+++ flask_oidc/__init__.py
@@ -139,6 +139,8 @@ class OpenIDConnect(object):
             self.keycloakApi = KeycloakAPI()
             self.keycloakApi.init_app(keycloak_secrets)
             self.keycloak_enabled = True
+            self.keycloak_realm_roles = None
+            self.keycloak_client_roles = None
         self.client_secrets = list(secrets.values())[0]
         secrets_cache = DummySecretsCache(secrets)
 
@@ -994,6 +996,8 @@ class OpenIDConnect(object):
             if json_content_rpt is None:
                 raise Exception("Not authorized!")
             resources = self._get_permissions_from_token(json_content_rpt["access_token"])
+            self.keycloak_realm_roles = self._get_realm_roles_from_token(json_content_rpt["access_token"])
+            self.keycloak_client_roles = self._get_client_roles_from_token(json_content_rpt["access_token"])
             if resources is None:
                 raise Exception("Empty resources set.")
             for resource_id in resources:
@@ -1005,6 +1009,25 @@ class OpenIDConnect(object):
             logger.debug(str(e))
         logger.debug("Not authorized!")
         return False
+
+    def get_keycloak_realm_roles(self):
+        return self.keycloak_realm_roles
+
+    def get_keycloak_client_roles(self):
+        return self.keycloak_client_roles
+
+    def _get_realm_roles_from_token(self, token):
+        token = self.keycloakApi.jwt_decode(token)
+        if (token is None) or ('realm_access' not in token) or ('roles' not in token["realm_access"]):
+            return None
+        return token["realm_access"]["roles"]
+
+
+    def _get_client_roles_from_token(self, token):
+        token = self.keycloakApi.jwt_decode(token)
+        if (token is None) or ('resource_access' not in token):
+            return None
+        return token["resource_access"]
 
     def _get_permissions_from_token(self, rpt_token):
         """
