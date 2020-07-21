e651254a15aaa7c88a192ab93aeb321b838273f6
vishnu667 <vishnu667@gmail.com>
2020-07-22 02:40:43 +0530

    test_commit

--- setup.py
+++ setup.py
@@ -4,30 +4,18 @@ import sys
 
 from setuptools import setup
 
-# This check is to make sure we checkout docs/_themes before running sdist
-# if not os.path.exists("./docs/_themes/README"):
-#     print('Please make sure you have docs/_themes checked out while running setup.py!')
-#     if os.path.exists('.git'):
-#         print('You seem to be using a git checkout, please execute the following commands to get the docs/_themes directory:')
-#         print(' - git submodule init')
-#         print(' - git submodule update')
-#     else:
-#         print('You seem to be using a release. Please use the release tarball from PyPI instead of the archive from GitHub')
-#     sys.exit(1)
-
-
 here = os.path.abspath(os.path.dirname(__file__))
 with io.open(os.path.join(here, 'README.rst')) as f:
     readme = f.read()
 
 setup(
-    name='flask-oidc',
+    name='flask-oidc2',
     description='OpenID Connect extension for Flask',
     long_description=readme,
     url='https://github.com/vishnu667/flask-oidc2',
     author='Vishnu Prasad, Patrick Uiterwijk',
     author_email='vishnu667@gmail.com',
-    version='1.4.1',
+    version='1.4.2-snapshot',
     packages=[
         'flask_oidc',
     ],
